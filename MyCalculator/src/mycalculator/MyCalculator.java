7f8626e617de0468df71073e4dfcd523f2f40c3d
JuanWang <lisawangjuan@gmail.com>
2018-02-14 14:57:04 -0800

    add main file

---
+++ MyCalculator/src/mycalculator/MyCalculator.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mycalculator;
+
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
+
+public class MyCalculator  {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        GUI myCal = new GUI();
+        myCal.init();
+        
+    }
+    
+}
